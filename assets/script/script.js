f9b2cfb20e466f7955c844d3d7b4d79d9523aac9
puakehaulani <puakehaulani@gmail.com>
2020-10-04 02:10:30 -0700

    all functions working

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,12 +2,32 @@
 let dateEl = document.getElementById("currentDay");
 let timeVar = moment();
 let currentTime = timeVar.format("HH"); //LT
-
+console.log(currentTime);
 dateEl.innerHTML = timeVar.format("dddd, MMMM Do");
 
 //Scheduler Pseudocode
 // stuff to do not necessarily in order
+// 3. Change background color styling on li element to match time
+// $("").replaceWith()
 
+// let hourBlock = $(".hour").attr('data-militaryTime');
+for (i = 1; i < 11; i++) {
+    let idName = "color-area-" + i;
+    let $div = $("input#" + idName);
+    let hourBlock = $div.attr('data-militaryTime');
+    console.log(idName);
+    // a. Use moment.js to check current time? create boolean variables on how the current time is related to the time block. Then if/else statements using the boolean values(?) to check against current time, and add in style changes.
+    if (currentTime < hourBlock) {
+        $div.addClass('future');
+        console.log("it is future" + hourBlock);
+    } else if (currentTime > hourBlock) {
+        $div.addClass('past');
+        console.log("it is past" + hourBlock);
+    } else if (currentTime === hourBlock) {
+        $div.addClass('present');
+        console.log("it is present" + hourBlock);
+    }
+}
 // a. Store input in local storage?
 $(".saveBtn").click(function (event) {
     console.log(event.target);
@@ -26,24 +46,6 @@ for (var i = 0; i < localStorage.length; i++) {
     console.log(localStorage.key(i));
     $("#" + keyName).parent().siblings(".text-input").val(valContent);
 }
-
-// 3. Change background color styling on li element to match time
-// $("").replaceWith()
-
-let hourBlock = $(this).data();
-console.log(hourBlock);
-// a. Use moment.js to check current time? create boolean variables on how the current time is related to the time block. Then if/else statements using the boolean values(?) to check against current time, and add in style changes.
-if (currentTime > hourBlock) {
-    $('hour').addClass('future').removeClass('hour');
-    console.log("it is" + hourBlock);
-} else if (currentTime < hourBlock) {
-    $('hour').addClass('past').removeClass('hour');
-    console.log("it is" + hourBlock);
-} else {
-    $('hour').addClass('present').removeClass('hour');
-    console.log("it is" + hourBlock);
-}
-
 
 // 4. EXTRA if time at the end, style nicely
 // Hints/Notes/Extra:
